589fda5c8130d211fa9a2e1ea0b2f03eccd91d5c
unknown <ivancampos039@gmail.com>
2022-10-19 19:54:23 -0300

    inicio do projeto secret word, um jogo de adivinhar palavras

---
+++ src/data/word.js
@@ -0,0 +1,8 @@
+export const WordsList = {
+    carro:["Motor", "Porta", "Capô", "Pneu", "Volante"],
+    fruta:["Banana", "Maçã", "Morango", "Mamão", "Laranja"],
+    corpo:["Cabeça", "Braço", "Perna","Olho","Boca"],
+    computador:["Mouse", "Teclado", "CPU", "Monitor", "Fonte" ],
+    programação:["Javascript", "React", "Flutter", "CSS", "Framework"],
+    alimento:["Arroz", "Feijão", "Batata", "Carne", "Ovo"],
+};
