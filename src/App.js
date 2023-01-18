ccbd03f8be4d6e60ee62c388e9b206c1514fc10b
Ivan Lourenco <ivancampos039@gmail.com>
2023-01-18 13:37:45 -0300

    novas funcoes no app

--- src/App.js
+++ src/App.js
@@ -23,8 +23,43 @@ function App() {
   const [gameStage, setGameStage] =useState(stages[0].name)
   const [words] = useState(WordsList);
 
+
+  const [pickedWorld, setPickedWorld] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState("");
+
+  const pickWordAndCategory = () => {
+    //Pick a random category
+    const categories = Object.keys(words)
+    const category = 
+    categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    console.log(category)  
+
+    //pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+    console.log(word);
+
+    return {word, category};
+  };
+
     //starts the secret word game
   const startGame = () => {
+    // pick word and pick category
+    const {word, category} = pickWordAndCategory();
+
+    //create an array of letters
+    let wordLetters  = word.split("");
+    wordLetters = wordLetters.map((minusculo) => minusculo.toLowerCase());
+
+    console.log(word,category);
+    console.log(wordLetters);
+
+    //fill states
+    setPickedWorld (word);
+    setPickedCategory (category);
+    setLetters(letters);
+    
     setGameStage(stages[1].name)
   };
 
